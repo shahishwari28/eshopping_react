b66337bf60cbbcdb6c422ee4b3b0096b3ff14aca
AAA <ishwari.shah>
2021-05-22 16:36:06 +0530

    updated shop data with new data,added util

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
